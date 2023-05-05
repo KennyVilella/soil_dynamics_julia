de2f339447723e9ac73f4108f3d6c84ac728f9e7
Kenny Vilella <kennyvilella@gmail.com>
2023-05-05 16:47:20 +0900

    [changed] better formatting

--- src/types.jl
+++ src/types.jl
@@ -1,10 +1,11 @@
 """
 Copyright, 2023,  Vilella Kenny.
 """
-
-##################################################################################
-########################### Grid properties struct ###############################
-##################################################################################
+#==========================================================================================#
+#                                                                                          #
+#                          Starting implementation of new types                            #
+#                                                                                          #
+#==========================================================================================#
 """
     GridParam{I<:Int64,T<:Float64}
 
@@ -30,14 +31,15 @@ Convention:
   while their height can potentially be lower
             AE = BF = CG = DH <= AB.
 - The center of each cell (O) is considered to be at the center of the top surface.
-- The considered reference frame follows the right-hand rule, with the Z direction pointing upward.
+- The considered reference frame follows the right-hand rule,
+  with the Z direction pointing upward.
 
 # Fields
 
 - `half_length_x::Int64`: Number of grid elements in the positive (or negative) X direction.
 - `half_length_y::Int64`: Number of grid elements in the positive (or negative) Y direction.
-- `half_length_z::Float64`: Number of grid elements in the positive (or negative) Z direction.
-                            Note that it is a Float.
+- `half_length_z::Float64`: Number of grid elements in the positive (or negative) Z
+                            direction. Note that it is a Float.
 - `cell_size_xy::Float64`: Size of the cells in the X and Y direction. [m]
 - `cell_size_z::Float64`: Height of the cells in the Z direction. [m]
 - `cell_area::Float64`: Surface area of the cells in the horizontal plane. [m^2]
@@ -46,7 +48,8 @@ Convention:
 # Inner constructor
 
     GridParam(
-        grid_size_x::T, grid_size_y::T, grid_size_z::T, cell_size_xy::T, cell_size_z::T=cell_size_xy
+        grid_size_x::T, grid_size_y::T, grid_size_z::T, cell_size_xy::T,
+        cell_size_z::T=cell_size_xy
     ) where {T<:Float64}
 
 Create a new instance of GridParam using the grid size in [m].
@@ -99,16 +102,24 @@ struct GridParam{I<:Int64,T<:Float64}
             throw(DomainError(grid_size_z, "grid_size_z should be greater than zero"))
         end
         if (cell_size_z > cell_size_xy)
-            throw(ErrorException("cell_size_z should be lower than or equal to cell_size_xy"))
+            throw(ErrorException(
+                "cell_size_z should be lower than or equal to cell_size_xy"
+            ))
         end
         if (grid_size_x < cell_size_xy)
-                throw(ErrorException("cell_size_xy should be lower than or equal to grid_size_x"))
+                throw(ErrorException(
+                    "cell_size_xy should be lower than or equal to grid_size_x"
+                ))
         end
         if (grid_size_y < cell_size_xy)
-            throw(ErrorException("cell_size_xy should be lower than or equal to grid_size_y"))
+            throw(ErrorException(
+                "cell_size_xy should be lower than or equal to grid_size_y"
+            ))
         end
         if (grid_size_z < cell_size_z)
-            throw(ErrorException("cell_size_z should be lower than or equal to grid_size_z"))
+            throw(ErrorException(
+                "cell_size_z should be lower than or equal to grid_size_z"
+            ))
         end
 
         half_length_x = round(Int64, grid_size_x / cell_size_xy)
@@ -119,8 +130,8 @@ struct GridParam{I<:Int64,T<:Float64}
         cell_volume = cell_area * cell_size_z
 
         new{Int64,T}(
-            half_length_x, half_length_y, half_length_z, cell_size_xy, cell_size_z, cell_area, cell_volume,
+            half_length_x, half_length_y, half_length_z, cell_size_xy, cell_size_z,
+            cell_area, cell_volume,
         )
     end
 end
-
