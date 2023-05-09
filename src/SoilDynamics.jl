3ddbf569212a2b924da0ed107249cf43222cd266
Kenny Vilella <kennyvilella@gmail.com>
2023-05-09 17:23:32 +0900

    [added] calc_triangle_pos function

--- src/SoilDynamics.jl
+++ src/SoilDynamics.jl
@@ -6,8 +6,8 @@ import LinearAlgebra: norm
 export GridParam
 
 # bucket.jl
-export _calc_rectangle_pos, _calc_line_pos
-export _decompose_vector_rectangle
+export _calc_rectangle_pos, _calc_triangle_pos, _calc_line_pos
+export _decompose_vector_rectangle, _decompose_vector_triangle
 
 # Files
 include("types.jl")
--- src/bucket.jl
+++ src/bucket.jl
@@ -267,6 +267,261 @@ function _decompose_vector_rectangle(
 end
 
 """
+    _calc_triangle_pos(
+        a::Vector{T}, b::Vector{T}, c::Vector{T},
+        delta::T, grid::GridParam{I,T}, tol::T=1e-8
+    ) where {I<:Int64,T<:Float64}
+
+This function determines the cells where a triangle surface is located. The triangle is
+defined by providing the Cartesian coordinates of its three vertices in the proper order.
+
+To optimize performance, the function iterates over a portion of the horizontal grid where
+the triangle is located. For each cell, the function calculates the height of the plane
+formed by the triangle at the top right corner of the cell. If the cell is within the
+triangle area, the calcualted height is added to the results for the four neighboring
+cells.
+
+This method works because when a plane intersects with a rectangular cell, the minimum and
+maximum height of the plane within the cell occurs at one of the cell corners.
+By iterating through all the cells, the function ensures that all the corners of each cell
+are investigated.
+
+However, this approach does not work when the triangle is perpendicular to the XY plane.
+To handle this case, the function uses the _calc_line_pos function to include the cells
+that lie on the three edges of the triangle.
+
+Note:
+- This function is intended for internal use only.
+- The iteration is performed over the top right corner of each cell, but any other corner
+  could have been chosen without affecting the results.
+- Not all cells are provided, since, at a given XY position, only the cells with the
+  minimum and maximum height are important.
+- When the triangle follows a cell border, the exact location of the triangle
+  becomes ambiguous. It is assumed that the caller resolves this ambiguity.
+
+# Inputs
+- `a::Vector{T}`: Cartesian coordinates of one vertex of the triangle. [m]
+- `b::Vector{T}`: Cartesian coordinates of one vertex of the triangle. [m]
+- `c::Vector{T}`: Cartesian coordinates of one vertex of the triangle. [m]
+- `delta::T`: Spatial increment used to decompose the edges of the triangle. [m]
+- `grid::GridParam{I,T}`: Struct that stores information related to the simulation grid.
+- `tol::T`: Small number used to handle numerical approximation errors.
+
+# Outputs
+- `Vector{Vector{Int64}}`: Collection of cells indices where the triangle is located.
+                           Result is not sorted and duplicates may be present.
+
+# Example
+
+    grid = GridParam(4.0, 4.0, 3.0, 0.05, 0.01)
+    a = [1.0, 0.0, 0.7]
+    b = [0.0, 1.0, 0.7]
+    c = [0.0, 1.0, 0.9]
+    tri_pos = _calc_triangle_pos(a, b, c, 0.01, grid)
+"""
+function _calc_triangle_pos(
+    a::Vector{T},
+    b::Vector{T},
+    c::Vector{T},
+    delta::T,
+    grid::GridParam{I,T},
+    tol::T=1e-8
+) where {I<:Int64,T<:Float64}
+
+    # Converting the three triangle vertices from position to indices
+    cell_size = [grid.cell_size_xy; grid.cell_size_xy; grid.cell_size_z]
+    grid_size = [grid.half_length_x + 1; grid.half_length_y + 1; grid.half_length_z + 1]
+    a_ind = a ./ cell_size .+ grid_size
+    b_ind = b ./ cell_size .+ grid_size
+    c_ind = c ./ cell_size .+ grid_size
+
+    # Calculating the bounding box of the triangle
+    area_min_x = floor(Int64, minimum([a_ind[1], b_ind[1], c_ind[1]]))
+    area_max_x = ceil(Int64, maximum([a_ind[1], b_ind[1], c_ind[1]]))
+    area_min_y = floor(Int64, minimum([a_ind[2], b_ind[2], c_ind[2]]))
+    area_max_y = ceil(Int64, maximum([a_ind[2], b_ind[2], c_ind[2]]))
+
+    # Calculating the lateral extent of the bounding box
+    area_length_x = area_max_x - area_min_x
+    area_length_y = area_max_y - area_min_y
+
+    # Calculating the basis formed by the triangle
+    ab = b - a
+    ac = c - a
+
+    # Converting the basis from position to indices
+    ab_ind = ab ./ cell_size
+    ac_ind = ac ./ cell_size
+
+    # Listing the cells inside the triangle area 
+    c_ab, c_ac, in_triangle, n_cell = _decompose_vector_triangle(
+        ab_ind, ac_ind, a_ind, area_min_x, area_min_y, area_length_x, area_length_y, tol
+    )
+
+    # Allocating memory
+    tri_pos = [Vector{Int64}(undef,3) for _ in 1:n_cell]
+
+    nn_cell = 1
+    # Determining all cells where the inner portion of the triangle area is located
+    for ii in area_min_x:area_max_x-1
+        for jj in area_min_y:area_max_y-1
+            # Calculating the indices corresponding to the previously calculated matrix
+            ii_s = ii - area_min_x + 1
+            jj_s = jj - area_min_y + 1
+
+            if (in_triangle[ii_s, jj_s])
+                ### Cell is inside the triangle area ###
+                # Calculating the height index of the triangle at this corner
+                kk = ceil(Int64,
+                    a_ind[3] + c_ab[ii_s, jj_s] * ab_ind[3] + c_ac[ii_s, jj_s] * ac_ind[3]
+                )
+
+                # Adding the four neighboring cells with the calculated height
+                tri_pos[nn_cell][1] = ii
+                tri_pos[nn_cell][2] = jj
+                tri_pos[nn_cell][3] = kk
+                tri_pos[nn_cell + 1][1] = ii + 1
+                tri_pos[nn_cell + 1][2] = jj
+                tri_pos[nn_cell + 1][3] = kk
+                tri_pos[nn_cell + 2][1] = ii
+                tri_pos[nn_cell + 2][2] = jj + 1
+                tri_pos[nn_cell + 2][3] = kk
+                tri_pos[nn_cell + 3][1] = ii + 1
+                tri_pos[nn_cell + 3][2] = jj + 1
+                tri_pos[nn_cell + 3][3] = kk
+
+                # Incrementing the index
+                nn_cell += 4
+            end
+        end
+    end
+
+    # Determining the cells where the three edges of the triangle are located
+    ab_pos = _calc_line_pos(a, b, delta, grid)
+    bc_pos = _calc_line_pos(b, c, delta, grid)
+    ca_pos = _calc_line_pos(c, a, delta, grid)
+
+    return [tri_pos; ab_pos; bc_pos; ca_pos]
+end
+
+"""
+    _decompose_vector_triangle(
+        ab_ind::Vector{T}, ac_ind::Vector{T}, a_ind::Vector{T},
+        area_min_x::I, area_min_y::I, area_length_x::I, area_length_y::I, tol::T=1e-8
+    ) where {I<:Int64,T<:Float64}
+
+This function performs a vector decomposition on a portion of the horizontal plane where
+a triangle ABC is located. The position of the triangle is defined by its edges AB and AC
+, while the specified area extends over [area_min_x, area_min_x + area_length_x]
+on the X direction and [area_min_y, area_min_y + area_length_y] on the Y direction.
+
+For each cell in the specified area, the function decomposes it into the basis formed by
+the vectors AB and AC. Let O be the name of a cell, it can then be decomposed as
+
+    AO = c_ab * AB + c_ac * AC.
+
+This decomposition leads to a system of 2 equations with 2 unknowns (c_ab and c_ac)
+
+AO[1] = c_ab * AB[1] + c_ac * AC[1] (1),
+AO[2] = c_ab * AB[2] + c_ac * AC[2] (2).
+
+One may note that AB[1] * (2) - AB[2] * (1) implies that
+
+AB[1] * AO[2] - AB[2] * AO[1] = c_ac * AC[2] * AB[1] - c_ac * AC[1] * AB[2]
+
+that can be further rewritten as
+
+c_ac = (AB[1] * AO[2] - AB[2] * AO[1]) / (AC[2] * AB[1] - AC[1] * AB[2]).
+
+Similarly, AC[1] * (2) - AC[2] * (1) implies that
+
+c_ab = -(AC[1] * AO[2] - AC[2] * AO[1]) / (AC[2] * AB[1] - AC[1] * AB[2]).
+
+This decomposition allows us to determine whether the cell O is inside the triangle area,
+since this requires c_ab and c_ac to be between 0 and 1, and the sum of c_ab and c_ac to be
+lower than 1.
+
+Note:
+- This function is intended for internal use only.
+
+# Inputs
+- `ab_ind::Vector{T}`: Indices representing the edge AB of the triangle.
+- `ac_ind::Vector{T}`: Indices representing the edge AC of the triangle.
+- `a_ind::Vector{T}`: Indices of the vertex A from which the edges AB and AC start.
+- `area_min_x::I`: Minimum index in the X direction of the specified area.
+- `area_min_y::I`: Minimum index in the Y direction of the specified area.
+- `area_length_x::I`: Number of grid elements in the X direction of the specified area.
+- `area_length_y::I`: Number of grid elements in the Y direction of the specified area.
+- `tol::T`: Small number used to handle numerical approximation errors.
+
+# Outputs
+- `c_ab::Matrix{Float64}`: Results of the vector decomposition in terms of the AB component.
+- `c_ac::Matrix{Float64}`: Results of the vector decomposition in terms of the AC component.
+- `in_triangle::Matrix{Bool}`: Indicates whether the cell is inside the triangle area.
+- `n_cell::Int64`: Number of cells inside the triangle area.
+
+# Example
+
+    ab_ind = [0, 4, 0]
+    ac_ind = [3, 0, 0]
+    a_ind = [10, 8, 25]
+    c_ab, c_ac, in_triangle, n_cell = _decompose_vector_triangle(
+        ab_ind, ac_ind, a_ind, 15, 12, 8, 7
+    )
+"""
+function _decompose_vector_triangle(
+    ab_ind::Vector{T},
+    ac_ind::Vector{T},
+    a_ind::Vector{T},
+    area_min_x::I,
+    area_min_y::I,
+    area_length_x::I,
+    area_length_y::I,
+    tol::T=1e-8
+) where {I<:Int64,T<:Float64}
+
+    # Allocating memory
+    c_ab = Matrix{Float64}(undef, area_length_x, area_length_y)
+    c_ac = Matrix{Float64}(undef, area_length_x, area_length_y)
+    in_triangle = Matrix{Bool}(undef, area_length_x, area_length_y)
+
+    # Setting constants for decomposing the cell position into the reference frame
+    c_ab_x = ac_ind[2] / (ab_ind[1] * ac_ind[2] - ab_ind[2] * ac_ind[1])
+    c_ab_y = ac_ind[1] / (ab_ind[1] * ac_ind[2] - ab_ind[2] * ac_ind[1])
+    c_ac_x = ab_ind[2] / (ab_ind[1] * ac_ind[2] - ab_ind[2] * ac_ind[1])
+    c_ac_y = ab_ind[1] / (ab_ind[1] * ac_ind[2] - ab_ind[2] * ac_ind[1])
+
+    # Preparation for the determination of the triangle position
+    # Iterating over the top right corner of all cells in the triangle area
+    n_cell = 0
+    for ii_s in 1:area_length_x
+        for jj_s in 1:area_length_y
+            # Calculating the indices corresponding to the simulation grid
+            ii = area_min_x - 0.5 + ii_s
+            jj = area_min_y - 0.5 + jj_s
+
+            # Decomposing the cell corner position into the basis formed by the triangle
+            c_ab[ii_s, jj_s] = c_ab_x * (ii - a_ind[1]) - c_ab_y * (jj - a_ind[2])
+            c_ac[ii_s, jj_s] = -c_ac_x * (ii - a_ind[1]) + c_ac_y * (jj - a_ind[2])
+
+            if (
+                (c_ab[ii_s, jj_s] > tol) && (c_ac[ii_s, jj_s] > tol) &&
+                (c_ab[ii_s, jj_s] + c_ac[ii_s, jj_s] < 1 - tol)
+            )
+                ### Cell is inside the triangle area ###
+                in_triangle[ii_s, jj_s] = true
+                n_cell += 4
+            else
+                ### Cell is outside the triangle area ###
+                in_triangle[ii_s, jj_s] = false
+            end
+        end
+    end
+
+    return c_ab, c_ac, in_triangle, n_cell
+end
+
+"""
     _calc_line_pos(
         a::Vector{T}, b::Vector{T}, delta::T, grid::GridParam{I,T}
     ) where {I<:Int64,T<:Float64}
