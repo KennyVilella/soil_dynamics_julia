8db728357b7bc23fdac3f2cf0e1b4ab3d9bcef50
KennyVilella <89772195+KennyVilella@users.noreply.github.com>
2023-05-20 17:08:13 +0900

    Add an example script for the simulator (#13)
    # Description
    - Add an example script

---
+++ test/example/test_soil_evolution.jl
@@ -0,0 +1,16 @@
+"""
+Copyright, 2023,  Vilella Kenny.
+"""
+
+include("soil_evolution.jl")
+#==========================================================================================#
+#                                                                                          #
+#                               Running the example script                                 #
+#                                                                                          #
+#==========================================================================================#
+# Setting parameters
+random_trajectory = false
+set_RNG = true
+
+# Launching the example script
+soil_evolution(random_trajectory, set_RNG)
