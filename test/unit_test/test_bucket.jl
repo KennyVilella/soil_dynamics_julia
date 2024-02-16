844a1dc57a2a508d9d8b3262e0ce111c961e5065
Kenny Vilella <kennyvilella@gmail.com>
2024-02-16 09:43:36 +0800

    [updated] bucket unit tests to use the reset_value_and_test function

--- test/unit_test/test_bucket.jl
+++ test/unit_test/test_bucket.jl
@@ -657,20 +657,10 @@ end
     @test (out.body[3][7, 10] ≈ -0.4) && (out.body[4][7, 10] ≈ 0.6)
 
     # Resetting bucket position
-    set_height(out, 6, 6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 7, 10, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 10, 9, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 9, 12, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    dropzeros!(out.body[1])
-    dropzeros!(out.body[2])
-    dropzeros!(out.body[3])
-    dropzeros!(out.body[4])
-
-    # Testing that no extra bucket position has been added
-    @test isempty(nonzeros(out.body[1]))
-    @test isempty(nonzeros(out.body[2]))
-    @test isempty(nonzeros(out.body[3]))
-    @test isempty(nonzeros(out.body[4]))
+    body_pos = [[1, 6, 6], [1, 7, 10], [3, 7, 10], [1, 9, 12], [1, 10, 9], [3, 10, 9]]
+    reset_value_and_test(
+        out, Vector{Vector{Int64}}(),  body_pos, Vector{Vector{Int64}}()
+    )
 end
 
 @testset "_update_body!" begin
@@ -717,23 +707,12 @@ end
     @test (out.body[3][10, 10] ≈ 0.1) && (out.body[4][10, 10] ≈ 0.4)
 
     # Resetting bucket position
-    set_height(out, 4, 4, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 5, 5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 6, 6, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 7, 11, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 7, 12, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 7, 13, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    set_height(out, 10, 10, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-    dropzeros!(out.body[1])
-    dropzeros!(out.body[2])
-    dropzeros!(out.body[3])
-    dropzeros!(out.body[4])
-
-    # Testing that no extra bucket position has been added
-    @test isempty(nonzeros(out.body[1]))
-    @test isempty(nonzeros(out.body[2]))
-    @test isempty(nonzeros(out.body[3]))
-    @test isempty(nonzeros(out.body[4]))
+    body_pos = [
+        [1, 4, 4], [1, 5, 5], [1, 6, 6], [1, 7, 11], [1, 7, 12], [1, 7, 13],
+        [1, 10, 10], [3, 10, 10], ]
+    reset_value_and_test(
+        out, Vector{Vector{Int64}}(),  body_pos, Vector{Vector{Int64}}()
+    )
 end
 
 @testset "_calc_bucket_pos!" begin
@@ -753,14 +732,11 @@ end
     @test (out.bucket_area[1, 1] == 7) && (out.bucket_area[1, 2] == 20)
     @test (out.bucket_area[2, 1] == 7) && (out.bucket_area[2, 2] == 15)
     # Resetting the bucket position
-    out.body[1][11:16, 11] .= 0.0
-    out.body[2][11:16, 11] .= 0.0
-    dropzeros!(out.body[1])
-    dropzeros!(out.body[2])
-    @test isempty(nonzeros(out.body[1]))
-    @test isempty(nonzeros(out.body[2]))
-    @test isempty(nonzeros(out.body[3]))
-    @test isempty(nonzeros(out.body[4]))
+    body_pos = [
+        [1, 11, 11], [1, 12, 11], [1, 13, 11], [1, 14, 11], [1, 15, 11], [1, 16, 11]]
+    reset_value_and_test(
+        out, Vector{Vector{Int64}}(),  body_pos, Vector{Vector{Int64}}()
+    )
 
     # Test: BP-CB-2
     bucket.j_pos_init .= Vector{Float64}([0.0, 0.0, 0.0])
@@ -774,13 +750,15 @@ end
     @test (out.bucket_area[1, 1] == 7) && (out.bucket_area[1, 2] == 20)
     @test (out.bucket_area[2, 1] == 5) && (out.bucket_area[2, 2] == 17)
     # Resetting the bucket position
-    out.body[1][11:16, 9:13] .= 0.0
-    dropzeros!(out.body[1])
-    dropzeros!(out.body[2])
-    @test isempty(nonzeros(out.body[1]))
-    @test isempty(nonzeros(out.body[2]))
-    @test isempty(nonzeros(out.body[3]))
-    @test isempty(nonzeros(out.body[4]))
+    body_pos = [
+        [1, 11, 9], [1, 12, 9], [1, 13, 9], [1, 14, 9], [1, 15, 9], [1, 16, 9],
+        [1, 11, 10], [1, 12, 10], [1, 13, 10], [1, 14, 10], [1, 15, 10], [1, 16, 10],
+        [1, 11, 11], [1, 12, 11], [1, 13, 11], [1, 14, 11], [1, 15, 11], [1, 16, 11],
+        [1, 11, 12], [1, 12, 12], [1, 13, 12], [1, 14, 12], [1, 15, 12], [1, 16, 12],
+        [1, 11, 13], [1, 12, 13], [1, 13, 13], [1, 14, 13], [1, 15, 13], [1, 16, 13]]
+    reset_value_and_test(
+        out, Vector{Vector{Int64}}(),  body_pos, Vector{Vector{Int64}}()
+    )
 
     # Test: BP-CB-3
     bucket.j_pos_init .= Vector{Float64}([0.0, 0.0, 0.0])
@@ -808,13 +786,13 @@ end
     @test (out.bucket_area[1, 1] == 2) && (out.bucket_area[1, 2] == 15)
     @test (out.bucket_area[2, 1] == 5) && (out.bucket_area[2, 2] == 17)
     # Resetting the bucket position
-    out.body[1][6:11, 9:13] .= 0.0
-    out.body[2][6:11, 9:13] .= 0.0
-    dropzeros!(out.body[1])
-    dropzeros!(out.body[2])
-    # Checking that no extra bucket position has been added
-    @test isempty(nonzeros(out.body[1]))
-    @test isempty(nonzeros(out.body[2]))
-    @test isempty(nonzeros(out.body[3]))
-    @test isempty(nonzeros(out.body[4]))
+    body_pos = [
+        [1, 6, 9], [1, 7, 9], [1, 8, 9], [1, 9, 9], [1, 10, 9], [1, 11, 9],
+        [1, 6, 10], [1, 7, 10], [1, 8, 10], [1, 9, 10], [1, 10, 10], [1, 11, 10],
+        [1, 6, 11], [1, 7, 11], [1, 8, 11], [1, 9, 11], [1, 10, 11], [1, 11, 11],
+        [1, 6, 12], [1, 7, 12], [1, 8, 12], [1, 9, 12], [1, 10, 12], [1, 11, 12],
+        [1, 6, 13], [1, 7, 13], [1, 8, 13], [1, 9, 13], [1, 10, 13], [1, 11, 13]]
+    reset_value_and_test(
+        out, Vector{Vector{Int64}}(),  body_pos, Vector{Vector{Int64}}()
+    )
 end
