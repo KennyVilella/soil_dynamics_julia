b34cd71f8843463e115b4e9633e173dd4bd5f7f6
Kenny Vilella <kennyvilella@gmail.com>
2024-02-17 10:02:38 +0800

    [updated] unit tests for the _update_body_soil! function

--- test/unit_test/test_body_soil.jl
+++ test/unit_test/test_body_soil.jl
@@ -37,305 +37,161 @@ out = SimOut(terrain, grid)
     bucket.pos[:] .= [0.0, 0.0, 0.0]
     bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
 
-    # Testing for a simple lateral translation from (11, 11) to (12, 11)
+    # Creating a function to reset the bucket pose
+    function _reset_bucket_pose(bucket)
+        bucket.pos[:] .= [0.0, 0.0, 0.0]
+        bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
+    end
+
+    # Test: BS-UBS-1
     pos = Vector{Float64}([cell_size_xy, 0.0, 0.0])
     ori = Quaternion([1.0, 0.0, 0.0, 0.0])
-    out.body[1][12, 11] = 0.0
-    out.body[2][12, 11] = 0.1
-    out.body_soil[1][11, 11] = 0.1
-    out.body_soil[2][11, 11] = 0.2
-    #push!(out.body_soil_pos, [1; 11; 11])
+    set_height(out, 12, 11, NaN, 0.0, 0.1, NaN, NaN, NaN, NaN, NaN, NaN)
+    set_height(out, 11, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    push_body_soil_pos(out, 1, 11, 11, [0.0, 0.0, 0.0], 0.1)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[1][12, 11] ≈ 0.1) && (out.body_soil[2][12, 11] ≈ 0.2)
-    #@test (out.body_soil_pos == [[1; 12; 11]])
-    out.body_soil[1][12, 11] = 0.0
-    out.body_soil[2][12, 11] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][12, 11] = 0.0
-    out.body[2][12, 11] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 12, 11, NaN, 0.1, 0.2, NaN, NaN)
+    check_body_soil_pos(out.body_soil_pos[1], 1, 12, 11, [0.0, 0.0, 0.0], 0.1)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[1, 12, 11]], [[1, 12, 11]])
 
-    # Testing for a simple lateral translation (2)
+    # Test: BS-UBS-2
     pos = Vector{Float64}([cell_size_xy, 0.0, 0.0])
     ori = Quaternion([1.0, 0.0, 0.0, 0.0])
-    out.body[1][12, 11] = 0.0
-    out.body[2][12, 11] = 0.1
-    out.body_soil[3][11, 11] = 0.1
-    out.body_soil[4][11, 11] = 0.2
-    #push!(out.body_soil_pos, [3; 11; 11])
+    set_height(out, 12, 11, NaN, 0.0, 0.1, NaN, NaN, NaN, NaN, NaN, NaN)
+    set_height(out, 11, 11, NaN, NaN, NaN, NaN, NaN, NaN, NaN, 0.1, 0.2)
+    push_body_soil_pos(out, 3, 11, 11, [0.0, 0.0, 0.0], 0.1)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[1][12, 11] ≈ 0.1) && (out.body_soil[2][12, 11] ≈ 0.2)
-    #@test (out.body_soil_pos == [[1; 12; 11]])
-    out.body_soil[1][12, 11] = 0.0
-    out.body_soil[2][12, 11] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][12, 11] = 0.0
-    out.body[2][12, 11] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 12, 11, NaN, 0.1, 0.2, NaN, NaN)
+    check_body_soil_pos(out.body_soil_pos[1], 1, 12, 11, [0.0, 0.0, 0.0], 0.1)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[1, 12, 11]], [[1, 12, 11]])
 
-    # Testing for a simple lateral translation (3)
+    # Test: BS-UBS-3
     pos = Vector{Float64}([cell_size_xy, 0.0, 0.0])
     ori = Quaternion([1.0, 0.0, 0.0, 0.0])
-    out.body[3][12, 11] = 0.0
-    out.body[4][12, 11] = 0.1
-    out.body_soil[1][11, 11] = 0.1
-    out.body_soil[2][11, 11] = 0.2
-    #push!(out.body_soil_pos, [1; 11; 11])
+    set_height(out, 12, 11, NaN, NaN, NaN, NaN, NaN, 0.0, 0.1, NaN, NaN)
+    set_height(out, 11, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    push_body_soil_pos(out, 1, 11, 11, [0.0, 0.0, 0.0], 0.1)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[3][12, 11] ≈ 0.1) && (out.body_soil[4][12, 11] ≈ 0.2)
-    #@test (out.body_soil_pos == [[3; 12; 11]])
-    out.body_soil[3][12, 11] = 0.0
-    out.body_soil[4][12, 11] = 0.0
-    dropzeros!(out.body_soil[3])
-    dropzeros!(out.body_soil[4])
-    @test isempty(nonzeros(out.body_soil[3]))
-    @test isempty(nonzeros(out.body_soil[4]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[3][12, 11] = 0.0
-    out.body[4][12, 11] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2)
+    check_body_soil_pos(out.body_soil_pos[1], 3, 12, 11, [0.0, 0.0, 0.0], 0.1)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[3, 12, 11]], [[3, 12, 11]])
 
-    # Testing for a simple lateral translation (4)
+    # Test: BS-UBS-4
     pos = Vector{Float64}([cell_size_xy, 0.0, 0.0])
     ori = Quaternion([1.0, 0.0, 0.0, 0.0])
-    out.body[3][12, 11] = 0.0
-    out.body[4][12, 11] = 0.1
-    out.body_soil[3][11, 11] = 0.1
-    out.body_soil[4][11, 11] = 0.2
-    #push!(out.body_soil_pos, [3; 11; 11])
+    set_height(out, 12, 11, NaN, NaN, NaN, NaN, NaN, 0.0, 0.1, NaN, NaN)
+    set_height(out, 11, 11, NaN, NaN, NaN, NaN, NaN, NaN, NaN, 0.1, 0.2)
+    push_body_soil_pos(out, 3, 11, 11, [0.0, 0.0, 0.0], 0.1)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[3][12, 11] ≈ 0.1) && (out.body_soil[4][12, 11] ≈ 0.2)
-    #@test (out.body_soil_pos == [[3; 12; 11]])
-    out.body_soil[3][12, 11] = 0.0
-    out.body_soil[4][12, 11] = 0.0
-    dropzeros!(out.body_soil[3])
-    dropzeros!(out.body_soil[4])
-    @test isempty(nonzeros(out.body_soil[3]))
-    @test isempty(nonzeros(out.body_soil[4]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[3][12, 11] = 0.0
-    out.body[4][12, 11] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2)
+    check_body_soil_pos(out.body_soil_pos[1], 3, 12, 11, [0.0, 0.0, 0.0], 0.1)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[3, 12, 11]], [[3, 12, 11]])
 
-    # Testing for a simple rotation from (12, 11) to (11, 12)
+    # Test: BS-UBS-5
     pos = Vector{Float64}([0.0, 0.0, 0.0])
     ori = angle_to_quat(-pi / 2, 0.0, 0.0, :ZYX)
-    out.body[1][11, 12] = 0.0
-    out.body[2][11, 12] = 0.1
-    out.body_soil[1][12, 11] = 0.1
-    out.body_soil[2][12, 11] = 0.2
-    #push!(out.body_soil_pos, [1; 12; 11])
+    set_height(out, 11, 12, NaN, 0.0, 0.1, NaN, NaN, NaN, NaN, NaN, NaN)
+    set_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    push_body_soil_pos(out, 1, 12, 11, [0.1, 0.0, 0.0], 0.1)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[1][11, 12] ≈ 0.1) && (out.body_soil[2][11, 12] ≈ 0.2)
-    #@test (out.body_soil_pos == [[1; 11; 12]])
-    out.body_soil[1][11, 12] = 0.0
-    out.body_soil[2][11, 12] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][11, 12] = 0.0
-    out.body[2][11, 12] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 11, 12, NaN, 0.1, 0.2, NaN, NaN)
+    check_body_soil_pos(out.body_soil_pos[1], 1, 11, 12, [0.1, 0.0, 0.0], 0.1)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[1, 11, 12]], [[1, 11, 12]])
 
-    # Testing for a simple rotation from (12, 11) to (12, 12)
+    # Test: BS-UBS-6
     pos = Vector{Float64}([0.0, 0.0, 0.0])
     ori = angle_to_quat(-pi / 4, 0.0, 0.0, :ZYX)
-    out.body[1][12, 12] = 0.0
-    out.body[2][12, 12] = 0.1
-    out.body_soil[1][12, 11] = 0.1
-    out.body_soil[2][12, 11] = 0.2
-    #push!(out.body_soil_pos, [1; 12; 11])
+    set_height(out, 12, 12, NaN, 0.0, 0.1, NaN, NaN, NaN, NaN, NaN, NaN)
+    set_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    push_body_soil_pos(out, 1, 12, 11, [0.1, 0.0, 0.0], 0.1)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[1][12, 12] ≈ 0.1) && (out.body_soil[2][12, 12] ≈ 0.2)
-    #@test (out.body_soil_pos == [[1; 12; 12]])
-    out.body_soil[1][12, 12] = 0.0
-    out.body_soil[2][12, 12] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][12, 12] = 0.0
-    out.body[2][12, 12] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 12, 12, NaN, 0.1, 0.2, NaN, NaN)
+    check_body_soil_pos(out.body_soil_pos[1], 1, 12, 12, [0.1, 0.0, 0.0], 0.1)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[1, 12, 12]], [[1, 12, 12]])
 
-    # Testing for a simple rotation + translation from (12, 11) to (13, 12)
+    # Test: BS-UBS-7
     pos = Vector{Float64}([cell_size_xy, 0.0, 0.0])
     ori = angle_to_quat(-pi / 4, 0.0, 0.0, :ZYX)
-    out.body[1][13, 12] = 0.0
-    out.body[2][13, 12] = 0.1
-    out.body_soil[1][12, 11] = 0.1
-    out.body_soil[2][12, 11] = 0.2
-    #push!(out.body_soil_pos, [1; 12; 11])
+    set_height(out, 13, 12, NaN, 0.0, 0.1, NaN, NaN, NaN, NaN, NaN, NaN)
+    set_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    push_body_soil_pos(out, 1, 12, 11, [0.1, 0.0, 0.0], 0.1)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[1][13, 12] ≈ 0.1) && (out.body_soil[2][13, 12] ≈ 0.2)
-    #@test (out.body_soil_pos == [[1; 13; 12]])
-    out.body_soil[1][13, 12] = 0.0
-    out.body_soil[2][13, 12] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][13, 12] = 0.0
-    out.body[2][13, 12] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 13, 12, NaN, 0.1, 0.2, NaN, NaN)
+    check_body_soil_pos(out.body_soil_pos[1], 1, 13, 12, [0.1, 0.0, 0.0], 0.1)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[1, 13, 12]], [[1, 13, 12]])
 
-    # Testing for a large transformation
+    # Test: BS-UBS-8
     pos = Vector{Float64}([0.0, 0.0, 0.0])
     ori = angle_to_quat(0.0, pi, 0.0, :ZYX)
-    out.body_soil[1][12, 11] = 0.1
-    out.body_soil[2][12, 11] = 0.2
-    #push!(out.body_soil_pos, [1; 12; 11])
-    _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    #@test out.terrain[10, 11] ≈ 0.1
-    out.terrain[10, 11] = 0.0
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][13, 12] = 0.0
-    out.body[2][13, 12] = 0.0
-    empty!(out.body_soil_pos)
+    set_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    push_body_soil_pos(out, 1, 12, 11, [0.1, 0.0, 0.0], 0.1)
+    warning_message = "WARNING\nBody soil could not be updated.\nSoil is moved to the " *
+        "terrain to maintain mass conservation."
+    @test_logs (:warn, warning_message) match_mode=:any _update_body_soil!(
+        out, pos, ori, grid, bucket
+    )
+    check_height(out, 10, 11, 0.1, NaN, NaN, NaN, NaN)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(
+        out, [[10, 11]], Vector{Vector{Int64}}(), Vector{Vector{Int64}}()
+    )
 
-    # Testing when two body_soil move to the same position (1)
+    # Test: BS-UBS-9
     pos = Vector{Float64}([0.0, 0.0, 0.0])
     ori = angle_to_quat(0.0, pi / 2, 0.0, :ZYX)
-    out.body[1][10, 11] = 0.0
-    out.body[2][10, 11] = 0.1
-    out.body_soil[1][12, 11] = 0.1
-    out.body_soil[2][12, 11] = 0.2
-    out.body_soil[1][13, 11] = 0.1
-    out.body_soil[2][13, 11] = 0.3
-    #push!(out.body_soil_pos, [1; 12; 11])
-    #push!(out.body_soil_pos, [1; 13; 11])
+    set_height(out, 11, 11, NaN, 0.0, 0.1, NaN, NaN, NaN, NaN, NaN, NaN)
+    set_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    set_height(out, 13, 11, NaN, NaN, NaN, 0.1, 0.3, NaN, NaN, NaN, NaN)
+    push_body_soil_pos(out, 1, 12, 11, [0.1, 0.0, 0.0], 0.1)
+    push_body_soil_pos(out, 1, 13, 11, [0.2, 0.0, 0.0], 0.2)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[1][10, 11] ≈ 0.1) && (out.body_soil[2][10, 11] ≈ 0.4)
-    #@test (out.body_soil_pos == [[1; 10; 11]])
-    out.body_soil[1][10, 11] = 0.0
-    out.body_soil[2][10, 11] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][10, 11] = 0.0
-    out.body[2][10, 11] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 11, 11, NaN, 0.1, 0.4, NaN, NaN)
+    check_body_soil_pos(out.body_soil_pos[1], 1, 11, 11, [0.1, 0.0, 0.0], 0.1)
+    check_body_soil_pos(out.body_soil_pos[2], 1, 11, 11, [0.2, 0.0, 0.0], 0.2)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[1, 11, 11]], [[1, 11, 11]])
 
-    # Testing when two body_soil move to the same position (2)
+    # Test: BS-UBS-10
     pos = Vector{Float64}([0.0, 0.0, 0.0])
     ori = angle_to_quat(0.0, pi / 2, 0.0, :ZYX)
-    out.body[1][10, 11] = 0.0
-    out.body[2][10, 11] = 0.1
-    out.body_soil[1][12, 11] = 0.1
-    out.body_soil[2][12, 11] = 0.2
-    out.body_soil[3][13, 11] = 0.1
-    out.body_soil[4][13, 11] = 0.3
-    #push!(out.body_soil_pos, [1; 12; 11])
-    #push!(out.body_soil_pos, [3; 13; 11])
+    set_height(out, 11, 11, NaN, 0.0, 0.1, NaN, NaN, NaN, NaN, NaN, NaN)
+    set_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    set_height(out, 13, 11, NaN, NaN, NaN, NaN, NaN, NaN, NaN, 0.1, 0.3)
+    push_body_soil_pos(out, 1, 12, 11, [0.1, 0.0, 0.0], 0.1)
+    push_body_soil_pos(out, 3, 13, 11, [0.2, 0.0, 0.0], 0.2)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[1][10, 11] ≈ 0.1) && (out.body_soil[2][10, 11] ≈ 0.4)
-    #@test (out.body_soil_pos == [[1; 10; 11]])
-    out.body_soil[1][10, 11] = 0.0
-    out.body_soil[2][10, 11] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[1][10, 11] = 0.0
-    out.body[2][10, 11] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 11, 11, NaN, 0.1, 0.4, NaN, NaN)
+    check_body_soil_pos(out.body_soil_pos[1], 1, 11, 11, [0.1, 0.0, 0.0], 0.1)
+    check_body_soil_pos(out.body_soil_pos[2], 1, 11, 11, [0.2, 0.0, 0.0], 0.2)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[1, 11, 11]], [[1, 11, 11]])
 
-    # Testing when two body_soil move to the same position (3)
+    # Test: BS-UBS-11
     pos = Vector{Float64}([0.0, 0.0, 0.0])
     ori = angle_to_quat(0.0, pi / 2, 0.0, :ZYX)
-    out.body[3][10, 11] = 0.0
-    out.body[4][10, 11] = 0.1
-    out.body_soil[1][12, 11] = 0.1
-    out.body_soil[2][12, 11] = 0.2
-    out.body_soil[3][13, 11] = 0.1
-    out.body_soil[4][13, 11] = 0.3
-    #push!(out.body_soil_pos, [1; 12; 11])
-    #push!(out.body_soil_pos, [3; 13; 11])
+    set_height(out, 11, 11, NaN, NaN, NaN, NaN, NaN, 0.0, 0.1, NaN, NaN)
+    set_height(out, 12, 11, NaN, NaN, NaN, 0.1, 0.2, NaN, NaN, NaN, NaN)
+    set_height(out, 13, 11, NaN, NaN, NaN, NaN, NaN, NaN, NaN, 0.1, 0.3)
+    push_body_soil_pos(out, 1, 12, 11, [0.1, 0.0, 0.0], 0.1)
+    push_body_soil_pos(out, 3, 13, 11, [0.2, 0.0, 0.0], 0.2)
     _update_body_soil!(out, pos, ori, grid, bucket)
-    # Checking body_soil
-    #@test (out.body_soil[3][10, 11] ≈ 0.1) && (out.body_soil[4][10, 11] ≈ 0.4)
-    #@test (out.body_soil_pos == [[3; 10; 11]])
-    out.body_soil[3][10, 11] = 0.0
-    out.body_soil[4][10, 11] = 0.0
-    dropzeros!(out.body_soil[1])
-    dropzeros!(out.body_soil[2])
-    @test isempty(nonzeros(out.body_soil[1]))
-    @test isempty(nonzeros(out.body_soil[2]))
-    # Checking terrain
-    @test all(out.terrain[:, :] .== 0.0)
-    # Resetting bucket position and orientation
-    bucket.pos[:] .= [0.0, 0.0, 0.0]
-    bucket.ori[:] .= [1.0, 0.0, 0.0, 0.0]
-    out.body[3][10, 11] = 0.0
-    out.body[4][10, 11] = 0.0
-    empty!(out.body_soil_pos)
+    check_height(out, 11, 11, NaN, NaN, NaN, 0.1, 0.4)
+    check_body_soil_pos(out.body_soil_pos[1], 3, 11, 11, [0.1, 0.0, 0.0], 0.1)
+    check_body_soil_pos(out.body_soil_pos[2], 3, 11, 11, [0.2, 0.0, 0.0], 0.2)
+    _reset_bucket_pose(bucket)
+    reset_value_and_test(out, Vector{Vector{Int64}}(),  [[3, 11, 11]], [[3, 11, 11]])
+
+    #======================================================================================#
+    # The tests below are specific to the current implementation and may become obsolete   #
+    # when the implementation change                                                       #
+    #======================================================================================#
+
+
 end
