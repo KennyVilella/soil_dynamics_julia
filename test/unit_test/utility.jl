7d89d092fbc15e991cfb32a0a7a7ef16407da99d
Kenny Vilella <kennyvilella@gmail.com>
2024-03-16 09:45:59 +0800

    [added] doxygen comments for all utility functions of unit tests

--- test/unit_test/utility.jl
+++ test/unit_test/utility.jl
@@ -9,6 +9,41 @@ using SoilDynamics
 #                                                                                          #
 #==========================================================================================#
 """
+    set_height(
+        out::SimOut{B,I,T}, ii::I, jj::I, terrain::T, body_1::T, body_2::T,
+        body_soil_1::T, body_soil_2::T, body_3::T, body_4::T, body_soil_3::T, body_soil_4::T
+    ) where {B<:Bool,I<:Int64,T<:Float64}
+
+This function set the height of the different layers at a given (`ii`, `jj`) position.
+`NaN` should be provided if no layer is present.
+
+# Note
+- This function is intended for unit testing only.
+
+# Inputs
+- `out::SimOut{Bool,Int64,Float64}`: Struct that stores simulation outputs.
+- `ii::Int64`: Index of the considered cell in the X direction.
+- `jj::Int64`: Index of the considered cell in the Y direction.
+- `terrain::Float64`: Height of the terrain. [m]
+- `body_1::Float64`: Minimum height of the first body layer. [m]
+- `body_2::Float64`: Maximum height of the first body layer. [m]
+- `body_soil_1::Float64`: Minimum height of the first body soil layer. [m]
+- `body_soil_2::Float64`: Maximum height of the first body soil layer. [m]
+- `body_3::Float64`: Minimum height of the second body layer. [m]
+- `body_4::Float64`: Maximum height of the second body layer. [m]
+- `body_soil_3::Float64`: Minimum height of the second body soil layer. [m]
+- `body_soil_4::Float64`: Maximum height of the second body soil layer. [m]
+
+# Outputs
+- None
+
+# Example
+
+    grid = GridParam(4.0, 4.0, 3.0, 0.05, 0.01)
+    terrain = zeros(2 * grid.half_length_x + 1, 2 * grid.half_length_y + 1)
+    out = SimOut(terrain, grid)
+
+    set_height(out, 10, 15, 0.5, NaN, NaN, 0.2, 0.3, 0.5, 0.6, NaN, NaN)
 """
 function set_height(
     out::SimOut{B,I,T},
@@ -60,6 +95,37 @@ function set_height(
 end
 
 """
+    check_height(
+        out::SimOut{B,I,T}, ii::I, jj::I, terrain::T, body_soil_1::T, body_soil_2::T,
+        body_soil_3::T, body_soil_4::T
+    ) where {B<:Bool,I<:Int64,T<:Float64}
+
+This function checks the height of the different layers at a given (`ii`, `jj`) position
+against the provided values. `NaN` should be provided if checking is not needed.
+
+# Note
+- This function is intended for unit testing only.
+
+# Inputs
+- `out::SimOut{Bool,Int64,Float64}`: Struct that stores simulation outputs.
+- `ii::Int64`: Index of the considered cell in the X direction.
+- `jj::Int64`: Index of the considered cell in the Y direction.
+- `terrain::Float64`: Height of the terrain. [m]
+- `body_soil_1::Float64`: Minimum height of the first body soil layer. [m]
+- `body_soil_2::Float64`: Maximum height of the first body soil layer. [m]
+- `body_soil_3::Float64`: Minimum height of the second body soil layer. [m]
+- `body_soil_4::Float64`: Maximum height of the second body soil layer. [m]
+
+# Outputs
+- None
+
+# Example
+
+    grid = GridParam(4.0, 4.0, 3.0, 0.05, 0.01)
+    terrain = zeros(2 * grid.half_length_x + 1, 2 * grid.half_length_y + 1)
+    out = SimOut(terrain, grid)
+
+    check_height(out, 10, 15, 0.5, NaN, NaN, 0.2, 0.3)
 """
 function check_height(
     out::SimOut{B,I,T},
@@ -93,6 +159,34 @@ function check_height(
 end
 
 """
+    reset_value_and_test(
+        out::SimOut{B,I,T}, terrain_pos::Vector{Vector{I}}, body_pos::Vector{Vector{I}},
+        body_soil_pos::Vector{Vector{I}}
+    ) where {B<:Bool,I<:Int64,T<:Float64}
+
+This function resets the requested outputs and checks that all `terrain`, `body` and
+`body_soil` is properly reset. This can be used to catch potential unexpected modifications
+of the outputs.
+
+# Note
+- This function is intended for unit testing only.
+
+# Inputs
+- `out::SimOut{Bool,Int64,Float64}`: Struct that stores simulation outputs.
+- `terrain_pos::Vector{Vector{Int64}}`: Collection of terrain cells that should be reset.
+- `body_pos::Vector{Vector{Int64}}`: Collection of body cells that should be reset.
+- `body_soil_pos::Vector{Vector{Int64}}`: Collection of body soil cells that should be reset.
+
+# Outputs
+- None
+
+# Example
+
+    grid = GridParam(4.0, 4.0, 3.0, 0.05, 0.01)
+    terrain = zeros(2 * grid.half_length_x + 1, 2 * grid.half_length_y + 1)
+    out = SimOut(terrain, grid)
+
+    reset_value_and_test((out, [[10, 15], [5, 7]], [[]], [[1, 15, 10], [3, 15, 10]])
 """
 function reset_value_and_test(
     out::SimOut{B,I,T},
@@ -154,6 +248,34 @@ function reset_value_and_test(
 end
 
 """
+    push_body_soil_pos(
+        out::SimOut{B,I,T}, ind::I, ii::I, jj::I, pos::Vecot{T}, h_soil::T
+    ) where {B<:Bool,I<:Int64,T<:Float64}
+
+This function pushes a new `BodySoil` struct into `body_soil_pos`.
+
+# Note
+- This function is intended for unit testing only.
+
+# Inputs
+- `out::SimOut{Bool,Int64,Float64}`: Struct that stores simulation outputs.
+- `ind::Int64`: Index of the soil layer.
+- `ii::Int64`: Index of the considered cell in the X direction.
+- `jj::Int64`: Index of the considered cell in the Y direction.
+- `pos::Vector{Float64}`: Cartesian coordinates of the body soil in the
+                          reference bucket frame. [m]
+- `h_soil::Float64`: Height of the soil column. [m]
+
+# Outputs
+- None
+
+# Example
+
+    grid = GridParam(4.0, 4.0, 3.0, 0.05, 0.01)
+    terrain = zeros(2 * grid.half_length_x + 1, 2 * grid.half_length_y + 1)
+    out = SimOut(terrain, grid)
+
+     push_body_soil_pos(out, 1, 10, 15, [0.1, 0.15, 0.0], 0.3)
 """
 function push_body_soil_pos(
     out::SimOut{B,I,T},
@@ -168,6 +290,35 @@ function push_body_soil_pos(
 end
 
 """
+    check_body_soil_pos(
+        body_soil_pos::BodySoil{I,T}, ind::I, ii::I, jj::I, pos::Vecot{T}, h_soil::T
+    ) where {I<:Int64,T<:Float64}
+
+This function checks the values of an inputted `BodySoil` struct against provided values.
+
+# Note
+- This function is intended for unit testing only.
+
+# Inputs
+- `body_soil_pos::BodySoil{Int64,Float64}`: `BodySoil` struct to be checked.
+- `ind::Int64`: Index of the soil layer.
+- `ii::Int64`: Index of the considered cell in the X direction.
+- `jj::Int64`: Index of the considered cell in the Y direction.
+- `pos::Vector{Float64}`: Cartesian coordinates of the body soil in the
+                          reference bucket frame. [m]
+- `h_soil::Float64`: Height of the soil column. [m]
+
+# Outputs
+- None
+
+# Example
+
+    grid = GridParam(4.0, 4.0, 3.0, 0.05, 0.01)
+    terrain = zeros(2 * grid.half_length_x + 1, 2 * grid.half_length_y + 1)
+    out = SimOut(terrain, grid)
+    push!(out.body_soil_pos, BodySoil(1, 10, 11, 0.1, 0.0, 0.2, 0.5))
+
+    check_body_soil_pos(out.body_soil_pos[1], 1, 10, 11, [0.1, 0.0, 0.2], 0.5)
 """
 function check_body_soil_pos(
     body_soil_pos::BodySoil{I,T},
