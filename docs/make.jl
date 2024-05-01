3a028d067a6fbd11883770a4209456bfc9cc091c
Kenny Vilella <kennyvilella@gmail.com>
2024-05-01 10:07:36 +0800

    [corrected] formatting in docs/make.jl following autoformatter

--- docs/make.jl
+++ docs/make.jl
@@ -2,20 +2,18 @@ using Documenter
 using SoilDynamics
 
 makedocs(
-  sitename = "Soil dynamics simulator",
-  authors = "Kenny Vilella",
-  pages    = [
-    "Home" => "index.md",
-    "types.jl" => "types.md",
-    "Grid" => "grid.md",
-    "bucket.jl" => "bucket.md",
-    "body_soil.jl" => "body_soil.md",
-    "intersecting_cells.jl" => "intersecting_cells.md",
-    "relax.jl" => "relax.md",
-    "utils.jl" => "utils.md",
-  ]
+    sitename = "Soil dynamics simulator",
+    authors = "Kenny Vilella",
+    pages = [
+        "Home" => "index.md",
+        "types.jl" => "types.md",
+        "Grid" => "grid.md",
+        "bucket.jl" => "bucket.md",
+        "body_soil.jl" => "body_soil.md",
+        "intersecting_cells.jl" => "intersecting_cells.md",
+        "relax.jl" => "relax.md",
+        "utils.jl" => "utils.md"
+    ]
 )
 
-deploydocs(
-  repo = "github.com/KennyVilella/soil_dynamics_julia.git"
-)
+deploydocs(repo = "github.com/KennyVilella/soil_dynamics_julia.git")
